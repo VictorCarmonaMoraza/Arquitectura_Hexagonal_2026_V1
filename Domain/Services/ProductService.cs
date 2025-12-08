17dd79db1cb4018723d7f62e33ff189038e7012d
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-08 20:00:59 +0100

    Implementacion de Puerto Primario

---
+++ Domain/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Ports.Primary;
+using Domain.Ports.Secundary;
+
+namespace Domain.Services
+{
+    public class ProductService : IService
+    {
+        private readonly IRepository _repository;
+
+
+        //Constructor
+        public ProductService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            var products = _repository.GetAll();
+            return products;
+        }
+
+        public void Register(string marca, decimal price)
+        {
+            //Creacion de un nuevo producto
+            var product = new Product(Guid.NewGuid(), marca, price);
+
+            //Guardamos el producto
+            _repository.Save(product);
+
+        }
+    }
+}
