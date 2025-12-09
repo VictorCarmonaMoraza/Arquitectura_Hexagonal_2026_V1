8b1c236104be44fe1034fa2e5cc69774297c8349
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-09 20:03:58 +0100

    Implementacion de puerto secundario en el nuevo adaptador

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Definimos la ruta del archivo JSON
 string pathFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "productApi.json");
 //.Services.AddTransient<Domain.Ports.Secundary.IRepository>(provider => new JsonRepository.ProductRepository(pathFile));
-builder.Services.AddTransient<Domain.Ports.Secundary.IRepository>(provider => new XMlRepository.XMLProductRepository());
+builder.Services.AddTransient<Domain.Ports.Secundary.IRepository>(provider => new XMLRepository.XMLProductRepository(pathFile));
 builder.Services.AddTransient<IService, ProductService>();
 
 builder.Services.AddEndpointsApiExplorer();
